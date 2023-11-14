7517a48a056f7e32b3ea3536c2f3fa0843d64594
cupcakes439 <adeolaakande439@gmail.com>
2023-11-14 17:19:01 +0100

    Made some changes in index.js

--- index.js
+++ index.js
@@ -6,22 +6,52 @@ const btn2 = document.querySelector("#btn2")
 const div3 = document.querySelector(".div3")
 const div4 = document.querySelector(".div4")
 
+const dollars = async ()=> {
+    const datas = await fetch()
+    const data = await datas.json()
+    return (data)
+}
+
+const dollarSigns = async ()=> {
+    const datas = await fetch()
+    const data = await datas.json()
+    return (data)
+}
+
+const nairas = async ()=> {
+    const datas = await fetch()
+    const data = await datas.json()
+    return (data)
+}
+
+const nairaSigns = async ()=> {
+    const datas = await fetch()
+    const data = await datas.json()
+    return (data)
+}
+
 radio1.addEventListener("click" , ()=> {
-    btn1.addEventListener("click" , ()=> {
-        div3.textContent = `You have ${input.value * 10}`
+    btn1.addEventListener("click" , async ()=> {
+        const naira = await nairas()
+        const nairaSign = await nairaSigns()
+        div3.textContent = `You have ${input.value * naira}${nairaSign}`
     })
-    btn2.addEventListener("click" , ()=> {
-        div4.textContent = `You have ${input.value}`
+    btn2.addEventListener("click" , async ()=> {
+        const dollarSign = await dollarSigns()
+        div4.textContent = `You have ${input.value}${dollarSign}`
         input.value = ""
     })
 })
 
 radio2.addEventListener("click" , ()=> {
-    btn2.addEventListener("click" , ()=> {
-        div4.textContent = `You have ${input.value * 10}`
+    btn2.addEventListener("click" , async ()=> {
+        const dollar = await dollars()
+        const dollarSign = await dollarSigns()
+        div4.textContent = `You have ${input.value * dollar}${dollarSign}`
     })
-    btn1.addEventListener("click" , ()=> {
-        div3.textContent = `You have ${input.value}`
+    btn1.addEventListener("click" , async ()=> {
+        const nairaSign = await nairaSigns()
+        div3.textContent = `You have ${input.value}${nairaSign}`
         input.value = ""
     })
 })
